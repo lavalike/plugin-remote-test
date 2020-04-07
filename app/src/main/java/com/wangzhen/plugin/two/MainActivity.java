1a52bedd1360cda2cf1c61e948a5a955bee56ce8
王震 <lavalike@yeah.net>
2020-04-07 10:59:34 +0800

    test permission

--- app/src/main/java/com/wangzhen/plugin/two/MainActivity.java
+++ app/src/main/java/com/wangzhen/plugin/two/MainActivity.java
@@ -1,9 +1,16 @@
 package com.wangzhen.plugin.two;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.wangzhen.plugin.two.base.BaseActivity;
 import com.wangzhen.statusbar.DarkStatusBar;
@@ -23,11 +30,23 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         findViewById(R.id.btn_dark_status).setOnClickListener(this);
         findViewById(R.id.btn_toast).setOnClickListener(this);
         findViewById(R.id.btn_recycler).setOnClickListener(this);
+        findViewById(R.id.btn_call).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_call:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1);
+                    } else {
+                        call();
+                    }
+                } else {
+                    call();
+                }
+                break;
             case R.id.btn_recycler:
                 startActivity(new Intent(getActivity(), RecyclerViewActivity.class));
                 break;
@@ -43,6 +62,24 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 }
                 isDark = !isDark;
                 break;
+        }
+    }
+
+    private void call() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:18368865748"));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                call();
+            } else {
+                Toast.makeText(getActivity(), "需要拨打电话权限", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
